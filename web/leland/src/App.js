3dc29b94ac05bf61e432401293359e90848e6ec8
Devin Ryan Riota <devin.riota@airyrooms.com>
2019-08-25 01:59:25 +0700

    add URL and routes

--- web/leland/src/App.js
+++ web/leland/src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './routes/Home'
 import Detail from './routes/Detail'
 import Summary from './routes/Summary'
+import Search from './routes/Search'
 
-import { DETAIL_PATH, SUMMARY_PATH } from './url'
+import { DETAIL_PATH, SUMMARY_PATH, SEARCH_TYPE_PATH, SEARCH_CATEGORY_PATH } from './url'
 
 import './App.scss'
 
@@ -16,6 +17,8 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path={DETAIL_PATH} component={Detail} />
         <Route exact path={SUMMARY_PATH} component={Summary} />
+        <Route exact path={SEARCH_TYPE_PATH} component={Search} />
+        <Route exact path={SEARCH_CATEGORY_PATH} component={Search} />
       </Switch>
     </Router>
   );
--- web/leland/src/url.js
+++ web/leland/src/url.js
@@ -1,2 +1,4 @@
 export const SUMMARY_PATH = '/summary/:price'
 export const DETAIL_PATH = '/detail/:id'
+export const SEARCH_TYPE_PATH = '/search?type=:type'
+export const SEARCH_CATEGORY_PATH = '/search?category=:category'
