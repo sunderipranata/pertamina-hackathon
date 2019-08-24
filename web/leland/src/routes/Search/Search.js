a5df73db05f1121d27d40e548943a909d220add9
Devin Ryan Riota <devin.riota@airyrooms.com>
2019-08-25 01:49:14 +0700

    change splash screen message, add search

---
+++ web/leland/src/routes/Search/Search.js
@@ -0,0 +1,123 @@
+import React, { Component, Fragment } from 'react'
+import ClassNames from 'classnames'
+import NumberFormat from 'react-number-format'
+
+import sample_tanah_1 from './assets/sample-tanah-1.jpg'
+import sample_tanah_2 from './assets/sample-tanah-2.jpg'
+import sample_tanah_3 from './assets/sample-tanah-3.jpg'
+import sample_tanah_4 from './assets/sample-tanah-4.jpg'
+import ic_location from '../../assets/ic-location.svg'
+import ic_time from '../../assets/ic-time.svg'
+import Countdown from '../../components/Countdown'
+import BottomSheet from '../../components/BottomSheet'
+import Navbar from '../../components/Navbar'
+
+import { thousandSeparator } from '../../utils/currency'
+
+import assetService from '../../services/AssetService'
+import bidService from '../../services/BidService'
+
+import { withRouter } from 'react-router-dom'
+
+import CardAssets from '../../components/CardAssets'
+
+import './Search.scss'
+
+class Search extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      category: null,
+      type: null,
+      getBy: '', //CATEGORY/TYPE,
+      assetDetails: []
+    }
+  }
+
+  componentWillMount() {
+    //get by query param
+    let category = ''
+    let type = ''
+
+    if(category !== null) {
+      this.setState({
+        category: category,
+        type: null,
+        getBy: 'CATEGORY'
+      })
+    } else {
+      this.setState({
+        category: null,
+        type: type,
+        getBy: 'TYPE'
+      })
+    }
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
+    let { getBy, category, type } = this.state
+
+    if(getBy == 'CATEGORY') {
+      assetService.getByCategory(category, (result) => {
+        if (result.success) {
+          console.log('result', result.data.data)
+          this.setState({
+            assetDetails: result.data.data
+          })
+        }
+      });
+    } else {
+      assetService.getByTye(type, (result) => {
+        if (result.success) {
+          console.log('result', result.data.data)
+          this.setState({
+            assetDetails: result.data.data
+          })
+        }
+      });
+    }
+  }
+
+  renderCardAssets() {
+    let { assetDetails } = this.state
+    let result = []
+    assetDetails.forEach((a) => {
+      let obj = (
+        <CardAssets
+          id = { a.id }
+          title= { a.name }
+          type= { a.type }
+          location= { a.city }
+          area={ a.type == 'TANAH' ? a.land_area : a.building_area }
+          bidders={ 101 }
+          suit= { a.category }
+          price={ a.start_price }
+          style = {{ marginBottom: '10' }}
+        />
+      )
+
+      result.push(obj)
+    })
+
+    return (
+      result
+    )
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Navbar title="Detail" />
+        <div className="ph-detail__container">
+          <section name="section-popular" className="container">
+            { this.renderCardAssets() }
+          </section>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default withRouter(Detail)
--- web/leland/src/services/AssetService.js
+++ web/leland/src/services/AssetService.js
@@ -92,12 +92,62 @@ assetService.getAuctionInfo = function(assetId, callback) {
     })
 }
 
+assetService.getByCategory = function(category, callback) {
+  request
+    .get(baseUrl + provideGetByCategoryPath(category))
+    .end(function (err, res) {
+      let result = {
+        success: '',
+        data: ''
+      }
+
+      if (err) {
+        console.log('error', err)
+        result.success = false
+      } else {
+        result.success = true
+        result.data = res.body
+      }
+
+      callback(result)
+    })
+}
+
+assetService.getByType = function(type, callback) {
+  request
+    .get(baseUrl + provideGetByTypePath(type))
+    .end(function (err, res) {
+      let result = {
+        success: '',
+        data: ''
+      }
+
+      if (err) {
+        console.log('error', err)
+        result.success = false
+      } else {
+        result.success = true
+        result.data = res.body
+      }
+
+      callback(result)
+    })
+}
+
 let provideGetAssetByIdPath = function (id) {
   return getAllAssetsPath + "/" + id;
 }
 
 let provideGetAuctionInfoByAssetIdPath = function(id) {
   return getAuctionInfoPath + "/" + id;
+}
+
+let provideGetByCategoryPath = function(category) {
+  return getRunningAssetsPath + "?category=" + category
+}
+
+let provideGetByTypePath = function(type) {
+  return getRunningAssetsPath + "?type=" + type
 }
 
 export default assetService
